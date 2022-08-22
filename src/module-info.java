bcc1fb08d389e9bf8bc9db1ce08e4228abf7751e
Sema Gözüacik <semagzckk@gmail.com>
2022-08-22 23:55:18 +0300

    sample run of the program is shown in

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,8 @@
 module Java_InarAcademy {
-}
+
+	public class checkpoint_01 {
+	        public static void main(String[] args) {
+	                double miles =100;
+	                final double  KILOMETERS_PER_MILE = 1.609;
+	                double kilometers = miles * KILOMETERS_PER_MILE;
+	                System.out.println(miles  + " miles equals to "+ kilometers+ " kilometers..");
