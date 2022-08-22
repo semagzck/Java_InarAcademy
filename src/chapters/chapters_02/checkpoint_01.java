6cd3a7c8ff2bbcb6b3772b95b18faf9a6651f83b
Sema Gözüacik <semagzckk@gmail.com>
2022-08-23 00:42:33 +0300

    Fixed a logic error

--- src/chapters/chapters_02/checkpoint_01.java
+++
@@ -1,8 +0,0 @@
-public class checkpoint_01{
-	public static void main(String[] a) {
-		double miles =100;
-		final double  KILOMETERS_PER_MILE = 1.609;
-		double kilometers = miles * KILOMETERS_PER_MILE ;
-		System.out.println( miles  + " miles equals to "+ kilometers + " kilometers..");
-	}
-}
