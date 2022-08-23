caf3f68881c446e96cf131e78318bc7b183cce0c
Sema Gözüacik <semagzckk@gmail.com>
2022-08-23 11:53:36 +0300

    convert miles to kilometres

--- src/chapters/chapter_02/checkpoint_01.java
+++ src/chapters/chapter_02/checkpoint_01.java
@@ -1,8 +1,11 @@
-public class checkpoint_01{
-	public static void main(String[] a) {
+package chapters.chapter_02;
+
+public class checkpoint_01 {
+	public static void main(String[] args) {
 		double miles =100;
 		final double  KILOMETERS_PER_MILE = 1.609;
-		double kilometers = miles * KILOMETERS_PER_MILE ;
-		System.out.println( miles  + " miles equals to "+ kilometers + " kilometers..");
+		double kilometers = miles * KILOMETERS_PER_MILE;
+		System.out.println(miles  + " miles equals to "+ kilometers+ " kilometers..");
 	}
 }
+
