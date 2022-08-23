fe35b0e0762e55ec3a18f5df801f09a7359b0bee
Sema Gözüacik <semagzckk@gmail.com>
2022-08-23 13:27:40 +0300

    Result of calculation

---
+++ src/chapters/chapter_02/checkpoint_03.java
@@ -0,0 +1,7 @@
+package chapters.chapter_02;
+public class checkpoint_03 {
+	public static void main(String[] args) {
+	System.out.println(2 * (5 / 2 + 5 / 2));
+	System.out.println(2 * 5 / 2 + 2 * 5 / 2);
+	System.out.println(2 * (5 / 2));
+	System.out.println(2 * 5 / 2);
