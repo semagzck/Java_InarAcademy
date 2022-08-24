b3b62e657b1ab24e07455c45d4a3b3a0e36c2ff3
Sema Gözüacik <semagzckk@gmail.com>
2022-08-24 23:29:51 +0300

    Compute the volume of a eyeliner

---
+++ src/chapters/chapter_02/exercises_02.java
@@ -0,0 +1,17 @@
+public class Exercise_02 {
+
+    public static void main(String[] Strings) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the radius and the length of a cylinder: ");
+        double radius = input.nextDouble();
+        double length = input.nextDouble();
+
+        double area = radius * radius * 3.14159265359;
+        double volume = area * length;
+
+        System.out.println("The area is " + area);
+        System.out.println("The volume is " + volume);
+             }
+}
