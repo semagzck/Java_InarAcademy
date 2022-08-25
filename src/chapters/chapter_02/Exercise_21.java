362ca99a6aaa0117af3c2573f52968e08c4aa876
Sema Gözüacik <semagzckk@gmail.com>
2022-08-25 23:27:21 +0300

    Financial application: moneraty units

---
+++ src/chapters/chapter_02/Exercise_21.java
@@ -0,0 +1,10 @@
+public class Exercise_21{
+ 
+ public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+ 
+  System.out.print("Enter investment amount:");
+  double investmentAmount = input.nextDouble();
+  System.out.print("Enter annual interest rate in percentage:");
+  double monthlyInterestRate = input.nextDouble();
+  monthlyInterestRate = monthly
