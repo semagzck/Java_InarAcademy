e490d084bed252bde2df5dcc4a0126fc1c664c8a
Sema Gözüacik <semagzckk@gmail.com>
2022-08-25 19:02:46 +0300

    Health application: computing BMI

---
+++ src/chapters/chapter_02/Exercise_14.java
@@ -0,0 +1,15 @@
+public class Exercise_14{
+ public static void main(String[] args) {
+ 
+  Scanner input = new Scanner(System.in);
+ 
+  System.out.print("Enter weight in pounds:");
+  double weight = input.nextDouble();
+  System.out.print("Enter height in inches:");
+  double height = input.nextDouble();
+  double BMI = (weight * 0.45359237) / Math.pow(height * 0.0254, 2);
+ 
+  System.out.print("BMI is " + BMI);
+ 
+ }
+}
