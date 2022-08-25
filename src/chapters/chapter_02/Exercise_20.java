2cb6edbc4326892233548b79ba1d0208681955df
Sema Gözüacik <semagzckk@gmail.com>
2022-08-25 23:20:43 +0300

    Financial application: calculate interest

---
+++ src/chapters/chapter_02/Exercise_20.java
@@ -0,0 +1,14 @@
+public class Exercise_20 {
+ 
+ public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+ 
+  System.out.print("Enter balance and interest rate (e.g., 3 for 3%):");
+  double balance = input.nextDouble();
+  double annualInterestRate = input.nextDouble();
+  double interest = balance * (annualInterestRate/1200);
+  System.out.print("The interest is " + interest);
+ 
+ }
+ 
+}
