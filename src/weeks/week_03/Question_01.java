4c4b70a39d1cefc5a2bdcaf738df26b3065eb264
Semanur <semagzckk@gmail.com>
2022-08-28 00:21:43 +0300

    Years of service and net bonus

---
+++ src/weeks/week_03/Question_01.java
@@ -0,0 +1,24 @@
+package weeks.week_03;
+import java.util.*;
+public class Question_01 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your service year :");
+		int year =input.nextInt();
+		System.out.println("Enter your salary :");
+		int salary =input.nextInt();
+		 
+		if(year>5) {
+			double bonus=salary * 0.05;
+			System.out.println("Your bonus amount is " + bonus);
+			
+		}
+		else {
+			System.out.println("Your bonus amount is 0 !!!");
+		}
+		
+	}
+
+}
+
+
