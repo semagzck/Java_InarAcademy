5ddffa6266928e4dfffa4298d1a4938c4050c17d
Semanur <semagzckk@gmail.com>
2022-08-29 20:45:14 +0300

    Game: heads or tails

--- src/chapters/chapter_03/Exercise_12.java
+++ src/chapters/chapter_03/Exercise_12.java
@@ -1,5 +1,24 @@
 package chapters.chapter_03;
+import java.util.Scanner;
 
 public class Exercise_12 {
-
+	 
+	 
+	 public static void main(String[] args) {
+	  Scanner input = new Scanner(System.in);
+	 
+	  System.out.print("Enter a three-digit integer:");
+	  int number = input.nextInt();
+	 
+	  // Extracting digits
+	  int first = number / 100;
+	  int last = number % 10;
+	  if (last == first) {
+	   System.out.print(number + " is a palindrome");
+	  } else {
+	   System.out.print(number + " is not a palindrome");
+	  }
+	 
+	 }
+	 
 }
