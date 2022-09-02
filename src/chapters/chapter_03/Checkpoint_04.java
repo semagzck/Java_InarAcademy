4a59e0b4e0b6cd80ea4a2d89ca01897363ee7fab
Semanur <semagzckk@gmail.com>
2022-09-02 13:47:23 +0300

    checkpoint_04

---
+++ src/chapters/chapter_03/Checkpoint_04.java
@@ -0,0 +1,25 @@
+package chapters.chapter03;
+
+import java.util.Scanner;
+
+public class Checkpoint_04 {
+
+	public static void main(String[] args) {
+		//SimpleIfDemo
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer: ");
+		int number = input.nextInt();
+		
+if(number % 5 == 0 ) {
+	System.out.println("Hi Five");
+		
+}if(number % 2 == 0) {
+			System.out.println("Hi Even");
+			System.out.println("Hello my friend.");
+		}
+
+	}
+
+}
+
