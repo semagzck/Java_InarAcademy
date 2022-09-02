d5d541f5e7404ac627ab8efb178b00dc68d56b51
Semanur <semagzckk@gmail.com>
2022-09-02 22:29:22 +0300

    vowel or consanant

---
+++ src/chapters/chapter_04/Exercise_13.java
@@ -0,0 +1,15 @@
+package chapters.chapter_04;
+
+public class Exercise_13 {
+
+	 public static void main(String[] args) {
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter a letter: ");
+	  String i = input.nextLine();
+	 
+	  if (i.length() != 1) {
+	   System.out.println("You must enter exactly one character");
+}
+	 }
+}
+
