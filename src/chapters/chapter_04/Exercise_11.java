531870166410097e79cbbd8d632e52902344dccf
Semanur <semagzckk@gmail.com>
2022-09-02 22:18:51 +0300

    decimal to hex

---
+++ src/chapters/chapter_04/Exercise_11.java
@@ -0,0 +1,21 @@
+package chapters.chapter_04;
+import java.util.Scanner;
+ 
+public class Exercise_11 {
+	 public static void main(String[] args) {
+		 System.out.print("Enter a decimal value (0 to 15): ");
+		  Scanner input = new Scanner(System.in);
+		  int i = input.nextInt();
+		  
+		  
+	   if (i < 0 || i > 15) {
+	    System.out.println(i + " is an invalid input");
+		
+	    System.exit(0);
+			  }
+	   System.out.println("The hex value is ");
+//??
+	 }
+
+}
+
