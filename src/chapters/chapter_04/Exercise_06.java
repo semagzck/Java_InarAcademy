73458e4c0997d4c8baa5c6561185714c32931987
Semanur <semagzckk@gmail.com>
2022-09-04 23:27:03 +0300

    random points on a circle

---
+++ src/chapters/chapter_04/Exercise_06.java
@@ -0,0 +1,18 @@
+package chapters.chapter_04;
+import java.util.Scanner;
+
+public class Exercise_04 {
+   public static void main(String[] args) {
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the side:");
+	 
+	  double s = input.nextDouble();
+	  double area = 6 * Math.pow(s, 2) / (4 * Math.tan(Math.PI / 6));
+	 
+	  area = Math.round(area * 100) / 100.0;
+	  System.out.println("The area of the hexagon is " + area);
+	 }
+	 
+	}
+
+
