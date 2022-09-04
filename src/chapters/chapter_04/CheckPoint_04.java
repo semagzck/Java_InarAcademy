0d58c2445b4bc853b0a1aa751531a445e846cf33
Semanur <semagzckk@gmail.com>
2022-09-05 00:16:13 +0300

    Write an expression that obtains a random integer between 34 and 55. Write an expression that obtains a random integer between 0 and 999. Write an expression that obtains a random number between 5.5 and 55.5

---
+++ src/chapters/chapter_04/CheckPoint_04.java
@@ -0,0 +1,7 @@
+Write an expression that obtains a random integer between 34 and 55. Write an expression that obtains a random integer between 0 and 999. Write an expression that obtains a random number between 5.5 and 55.5.
+
+       (a) 34 + (int)(Math.random() * (55 - 34)i)
+
+       (b) (int)(Math.random() * 1000)
+
+       (c) 5.5 + (Math.random() * (55.5 - 5.5))
