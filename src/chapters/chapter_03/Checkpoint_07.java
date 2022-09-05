5ff1fac8c13c814231daf825a029ac2bfe5e4fcc
Semanur <semagzckk@gmail.com>
2022-09-05 22:19:51 +0300

    checkpoint_07

---
+++ src/chapters/chapter_03/Checkpoint_07.java
@@ -0,0 +1,16 @@
+package chapters.chapter03;
+
+public class Checkpoint_07{
+
+	public static void main(String[] args) {
+		//Computer speed test.
+		for (int i = 0; i < 1_000_000; i++) {
+			System.out.println(System.currentTimeMillis());
+		}
+
+	
+		
+	}
+
+}
+
