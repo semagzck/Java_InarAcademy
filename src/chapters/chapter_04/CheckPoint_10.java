4f5747c91582e8640ac5095373a0760ccc563f9d
Semanur <semagzckk@gmail.com>
2022-09-05 22:02:30 +0300

    Suppose that s1, s2, and s3 are three strings, given as follows

---
+++ src/chapters/chapter_04/CheckPoint_10.java
@@ -0,0 +1,26 @@
+String s1 = "Welcome to Java";
+String s2 =  "Programming is fun";
+String s3 =  "Welcome to Java";
+What are the results of the following expressions?
+a.	s1 == s2
+b.	s2 == s3
+c.	s1.equals(s2) 
+d.	s1.equals(s3) 
+e.	s1.compareTo(s2) 
+f.	s2.compareTo(s3) 
+g.	s2.compareTo(s2) 
+h.	s1.charAt(0)
+i.	s1.indexOf('j')
+j.	s1.indexOf("to")
+k.	s1.lastIndexOf('a')
+l.	s1.lastIndexOf("o", 15)
+m.	s1.length()
+n.	s1.substring(5)
+o.	s1.substring(5, 11)
+p.	s1.startsWith("Wel")
+q.	s1.endsWith("Java")
+r.	s1.toLowerCase()
+s.	s1.toUpperCase()
+t.	s1.concat(s2)
+u.	s1.contains(s2)
+v.	"\t Wel \t".trim()
