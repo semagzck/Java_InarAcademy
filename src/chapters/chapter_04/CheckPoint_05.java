8b582f11be22bc66b7a4293c3aba3f03734469d0
Semanur <semagzckk@gmail.com>
2022-09-05 21:49:10 +0300

    ASCII code for 1, A, B, a, and b/ decimal codes 40, 59, 79, 85, and 90/hexadecimal code 40, 5A, 71, 72, and 7A

---
+++ src/chapters/chapter_04/CheckPoint_05.java
@@ -0,0 +1,20 @@
+
+Use print statements to find out the ASCII code for '1', 'A', 'B', 'a', and 'b'. Use print statements to find out the character for the decimal codes 40, 59, 79, 85, and 90. Use print statements to find out the character for the hexadecimal code 40, 5A, 71, 72, and 7A.
+
+System.out.println((int)'1');
+System.out.println((int)'A');
+System.out.println((int)'B');
+System.out.println((int)'a');
+System.out.println((int)'b');
+
+System.out.println((char)40);
+System.out.println((char)59);
+System.out.println((char)79);
+System.out.println((char)85);
+System.out.println((char)90);
+
+System.out.println((char)0X40);
+System.out.println((char)0X5A);
+System.out.println((char)0X71);
+System.out.println((char)0X72);
+System.out.println((char)0X7A);
