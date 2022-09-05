57593f716e58528f012581d95978d04aae1837bb
Semanur <semagzckk@gmail.com>
2022-09-05 21:56:20 +0300

    converted result

---
+++ src/chapters/chapter_04/CheckPoint_07.java
@@ -0,0 +1,13 @@
+Can the following conversions involving casting be allowed? If so, find the converted result.
+
+char c = 'A';
+int i = (int)c;
+
+float f = 1000.34f;
+int i = (int)f;
+
+double d = 1000.34;
+int i = (int)d;
+
+int i = 97;
+char c = (char)i;
