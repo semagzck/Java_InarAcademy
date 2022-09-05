95cf90e70349eb8a5679d558f02f3b896770b2eb
Semanur <semagzckk@gmail.com>
2022-09-05 22:04:42 +0300

    Show the output of the following statements (write a program to verify your results)

---
+++ src/chapters/chapter_04/CheckPoint_12.java
@@ -0,0 +1,6 @@
+Show the output of the following statements (write a program to verify your results):
+System.out.println("1" + 1);
+System.out.println('1' + 1);
+System.out.println("1" + 1 + 1);
+System.out.println("1" + (1 + 1));
+System.out.println('1' + 1 + 1);
