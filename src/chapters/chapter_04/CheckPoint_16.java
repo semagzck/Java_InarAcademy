7b00b67fb1fec5b5d90348540b9cbbec336d6ce1
Semanur <semagzckk@gmail.com>
2022-09-05 22:11:46 +0300

    output of expressions

---
+++ src/chapters/chapter_04/CheckPoint_16.java
@@ -0,0 +1,8 @@
+(a) System.out.printf("amount is %f %e\n", 32.32, 32.32);
+(b) System.out.printf("amount is %5.2f%% %5.4e\n", 32.327, 32.32);
+(c) System.out.printf("%6b\n", (1 > 2));
+(d) System.out.printf("%6s\n", "Java");
+(e) System.out.printf("%-6b%s\n", (1 > 2), "Java");
+(f) System.out.printf("%6b%-8s\n", (1 > 2), "Java");
+(g) System.out.printf("%,5d %,6.1f\n", 312342, 315562.932);
+(h) System.out.printf("%05d %06.1f\n", 32, 32.32);
