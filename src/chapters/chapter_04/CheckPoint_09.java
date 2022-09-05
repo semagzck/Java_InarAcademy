8c493b8f9186f394c1290116d83e225a1011e713
Semanur <semagzckk@gmail.com>
2022-09-05 22:00:34 +0300

    Show the output of the following statements

---
+++ src/chapters/chapter_04/CheckPoint_09.java
@@ -0,0 +1,7 @@
+Show the output of the following statements:
+System.out.println('a' < 'b');
+System.out.println('a' <= 'A');
+System.out.println('a' > 'b');
+System.out.println('a' >= 'A');
+System.out.println('a' == 'a');
+System.out.println('a' != 'b');
