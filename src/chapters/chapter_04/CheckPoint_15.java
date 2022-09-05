dc534657d53c3242a68ea426b88814c9972cd8a9
Semanur <semagzckk@gmail.com>
2022-09-05 22:09:35 +0300

    wrong statement

---
+++ src/chapters/chapter_04/CheckPoint_15.java
@@ -0,0 +1,5 @@
+(a) System.out.printf("%5d %d\n", 1, 2, 3);
+(b) System.out.printf("%5d %f\n", 1);
+(c) System.out.printf("%5d %f\n", 1, 2);
+(d) System.out.printf("%.2f\n%0.3f\n", 1.23456, 2.34);
+(e) System.out.printf("%08s\n", "Java");
