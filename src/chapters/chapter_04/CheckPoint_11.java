b5e5794cdb962a31bb3d20c7a944ed5b10ad9098
Semanur <semagzckk@gmail.com>
2022-09-05 22:03:37 +0300

    Suppose that s1 and s2 are two strings. Which of the following statements or expressions are incorrect

---
+++ src/chapters/chapter_04/CheckPoint_11.java
@@ -0,0 +1,10 @@
+String s = "Welcome to Java";
+String s3 = s1 + s2;
+String s3 = s1 - s2;
+s1 == s2;
+s1 >= s2;
+s1.compareTo(s2);
+int i = s1.length();
+char c = s1(0);
+char c = s1.charAt(s1.length());
+
