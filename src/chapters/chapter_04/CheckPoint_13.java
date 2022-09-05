76eaba35ae0bb9598d9d31d48db085ad43f5f0c6
Semanur <semagzckk@gmail.com>
2022-09-05 22:05:37 +0300

    Evaluate the following expressions (write a program to verify your results)

---
+++ src/chapters/chapter_04/CheckPoint_13.java
@@ -0,0 +1,5 @@
+Evaluate the following expressions (write a program to verify your results):
+1 + "Welcome " + 1 + 1
+1 + "Welcome " + (1 + 1)
+1 + "Welcome " + ('\u0001' + 1)
+1 + "Welcome " + 'a' + 1
