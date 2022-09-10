25cc16258ca77ed4526c33d5fb63f435b628156c
Semanur <semagzckk@gmail.com>
2022-09-10 13:08:05 +0300

    checkpoint_06

---
+++ src/chapters/chapter_02/checkpoint_06.java
@@ -0,0 +1,9 @@
+package chapters.chapter_02;
+public class checkpoint_06 {
+	public static void main(String[] args) {
+	double m= 5;
+	double r = 8.6;
+	System.out.println(m * Math.pow(r ,2));
+	}
+}
+
