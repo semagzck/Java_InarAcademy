5b2e00e0384506901ab731c23b863b36333a640c
Semanur <semagzckk@gmail.com>
2022-09-11 18:10:27 +0300

    output loop

---
+++ src/chapters/chapter_05/checkPoint_02.java
@@ -0,0 +1,18 @@
+//How many times are the following loop bodies repeated? What is the output of each loop?
+(a)
+int i = 1;
+while (i < 10)
+  if (i % 2 == 0)
+    System.out.println(i);
+
+(b)
+int i = 1;
+while (i < 10)
+  if (i % 2 == 0)
+    System.out.println(i++);
+
+(c)
+int i = 1;
+while (i < 10)
+  if ((i++) % 2 == 0)
+    System.out.println(i); 
