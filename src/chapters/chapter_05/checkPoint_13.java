4b26fc993917ba5aa249008742cab863fd62e913
Semanur <semagzckk@gmail.com>
2022-09-12 22:38:44 +0300

    corrected codes

---
+++ src/chapters/chapter_05/checkPoint_13.java
@@ -0,0 +1,19 @@
+public class checkPoint_13{
+ public void main(String[] args) {
+ for (int i = 0; i < 10; i++);
+sum += i;
+
+     if (i < j);
+ System.out.println(i)
+       else
+         System.out.println(j);
+      while (j < 10);
+      {
+        j++;
+     }
+
+     do {
+       j++;
+      } while (j < 10)
+    }
+  }
