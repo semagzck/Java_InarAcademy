d6683b8584d794dc3b29a889c52da91bcc25d4e6
Semanur <semagzckk@gmail.com>
2022-09-12 22:19:21 +0300

    output of the code

---
+++ src/chapters/chapter_05/checkPoint_04.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class checkPoint_04 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int number, max;
+    number = input.nextInt();
+    max = number;
+
+    while (number != 0) {
+      number = input.nextInt();
+      if (number > max)
+        max = number; 
+    }
+
+    System.out.println("max is " + max);
+    System.out.println("number " + number);
+  }
+}
