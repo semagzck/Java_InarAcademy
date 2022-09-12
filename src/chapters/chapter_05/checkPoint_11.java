5519b487f3ced7b8f7f0f194e9ca01f6654ffb70
Semanur <semagzckk@gmail.com>
2022-09-12 22:35:03 +0300

    Count the number of iterations in the following loops

---
+++ src/chapters/chapter_05/checkPoint_11.java
@@ -0,0 +1,23 @@
+Count the number of iterations in the following loops.
+(a)
+int count = 0;
+while (count < n) {
+  count++;
+}
+
+(b)
+for (int count = 0;
+  count <= n; count++) {
+}
+
+(c)
+int count = 5;
+while (count < n) {
+  count++;
+}
+
+(d)
+int count = 5;
+while (count < n) {
+  count = count + 3;
+}
