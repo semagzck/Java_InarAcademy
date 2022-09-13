3bf2aacc9db842792d86f3946747987ffbf3eee2
Semanur <semagzckk@gmail.com>
2022-09-13 21:43:51 +0300

    Show errors

---
+++ src/chapters/chapter_05/checkPoint_14.java
@@ -0,0 +1,17 @@
+public class checkPoint_14{
+ public static void main(String[] args) {
+ int i = 0;
+ do {
+ System.out.println(i + 4);
+ i++;
+ }
+ while (i < 10)
+ }
+ }
+
+ (b)
+ public class ShowErrors {
+   public static void main(String[] args) {
+    for (int i = 0; i < 10; i++);
+      System.out.println(i + 4);
+ }
