8406a0186be666d73e88cb126d3a7daa91f97caa
Semanur <semagzckk@gmail.com>
2022-09-13 21:49:59 +0300

    displays hex number

---
+++ src/chapters/chapter_05/checkPoint_17.java
@@ -0,0 +1,6 @@
+ System.out.println("The hex number is " + (hex.length() == 0 ? "0" : hex));
+or
+if (hex.length() == 0)
+  System.out.println("The hex number is 0");
+else
+  System.out.println("The hex number is " + hex);
