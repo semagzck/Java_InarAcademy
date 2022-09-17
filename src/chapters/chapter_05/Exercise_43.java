e4d2cc8a6b26e67cbb0549382f526226dc092648
Semanur <semagzckk@gmail.com>
2022-09-17 14:26:05 +0300

    Math: combinations

---
+++ src/chapters/chapter_05/Exercise_43.java
@@ -0,0 +1,17 @@
+package Chapters.chapter_05;
+import java.util.Scanner;
+
+public class Exercise_43 {
+	public static void main(String[] args) {
+
+	int total = 0;
+	for(int i = 1; i <= 7; i++) {
+	for(int j = i + 1; j <= 7; j++) {
+		System.out.println(i + " " + j);
+			total++;
+	}
+	}
+	System.out.println("The total number of all combinations is " + total);
+	
+}
+}
