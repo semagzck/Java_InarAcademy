9a2c496752ce0377c94f381ea9652b95f1a53cd2
Semanur <semagzckk@gmail.com>
2022-09-17 14:18:18 +0300

    Compute pi

---
+++ src/chapters/chapter_05/Exercise_25.java
@@ -0,0 +1,35 @@
+package Chapters.chapter_05;
+
+public class Exercise_25 {
+//compute pi
+	public static void main(String[] args) {
+		double pi0=0;
+		int a= 10_000;
+		int b= 20_000;
+		int c= 100_000;
+		for(int i=1;i<=a;i++) {
+			double d = Math.pow(-1,i+1 );
+			double e = (2*i-1);
+			pi0 +=d/e;;
+		}
+		System.out.println(" pi number for i=10000 ;" + 4*pi0);
+		double pi1=0;
+		for(int i=1;i<=b;i++) {
+			double d=Math.pow(-1,i+1 );
+			double e=(2*i-1);
+			pi1 += d/e;;
+			
+		}
+		System.out.println("pi number for i=20000 ;" + 4*pi1);
+		double pi2 = 0;
+		for(int i=1;i <= c; i++) {
+			double d=Math.pow(-1,i+1 );
+			double e=(2*i-1);
+			pi2 +=d/e;;
+		}
+		System.out.println("pi number for i=100000 ;" + 4*pi2);
+		
+		
+	}
+}
+
