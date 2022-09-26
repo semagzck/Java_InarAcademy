0ec75466945a1ee89f255950a4b0255cdef2da7c
Semanur <semagzckk@gmail.com>
2022-09-26 21:18:14 +0300

    Given two method definitions

---
+++ src/chapters/chapter_06/checkpoint_05.java
@@ -0,0 +1,6 @@
+public static double m(double x, double y)
+public static double m(int x, double y)
+tell which of the two methods is invoked for:
+a.	double z = m(4, 5);
+b.	double z = m(4, 5.4);
+c.	double z = m(4.5, 5.4);
