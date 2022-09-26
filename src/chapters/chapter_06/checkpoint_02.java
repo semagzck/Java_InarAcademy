cf9711ca4812742bf979b7e1f52695c76b384a87
Semanur <semagzckk@gmail.com>
2022-09-26 21:11:38 +0300

    Identify and correct the errors in the following program

---
+++ src/chapters/chapter_06/checkpoint_02.java
@@ -0,0 +1,11 @@
+  public class checkpoint_02 {
+      public static void main(String[] args) {
+        nPrintln(5, "Welcome to Java!");
+      }
+ 
+      public static void nPrintln(String message, int n) {
+        int n = 1;
+        for (int i = 0; i < n; i++)
+          System.out.println(message);
+     }
+  }
