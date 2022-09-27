0c632b564f484c8610f273967e23b5b8241e30e0
Semanur <semagzckk@gmail.com>
2022-09-27 22:06:04 +0300

    Sort three numbers

---
+++ src/chapters/chapter_06/Exercise_05.java
@@ -0,0 +1,7 @@
+package chapter_06;
+
+public class Exercise_05 {
+    System.out.print("Enter three numbers to sort: ");
+	displaySortedNumbers(in.nextDouble(), in.nextDouble(), in.nextDouble());
+}
+
