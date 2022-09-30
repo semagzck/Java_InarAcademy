e1ab9c62a0a492372adb579fac9d79df8a84b77d
Semanur <semagzckk@gmail.com>
2022-09-30 23:10:01 +0300

    Display an integer reserved

---
+++ src/chapters/chapter_06/Exercise_04.java
@@ -0,0 +1,25 @@
+
+	package chapter_06;
+	import java.util.Scanner;
+
+	public class hoppala {
+	    public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+	        System.out.print("Bir sayi giriniz: ");
+	        int number = input.nextInt();
+	        System.out.print("Girdiğiniz sayinin tersten yazilisi = ");
+	        reverse (number);
+	        
+	        }
+	        public static void reverse(int number) {
+	            while (number > 0) {
+	                System.out.print(number % 10);
+	                number /= 10;
+	            }
+
+	        }
+
+	}
+
+
+
