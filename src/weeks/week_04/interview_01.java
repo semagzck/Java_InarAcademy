b8ceef13409bc34eb8482fe32c52b77f5c6ab765
Semanur <semagzckk@gmail.com>
2022-10-03 10:37:58 +0300

    interview

---
+++ src/weeks/week_04/interview_01.java
@@ -0,0 +1,18 @@
+package weeks.week_04;
+
+public class Interview_01 {
+
+	public static void main(String[] args) {
+		int a = -25;
+		
+		//changing int to string.
+		
+		String b = a + "";
+		
+		System.out.println(b);
+		
+	}
+
+}
+Footer
+© 2022 GitHub, Inc.
