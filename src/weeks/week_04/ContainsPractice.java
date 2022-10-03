8abebc6d18e552a93330d98188cc58a28ab684fa
Semanur <semagzckk@gmail.com>
2022-10-03 10:35:52 +0300

    setting

---
+++ src/weeks/week_04/ContainsPractice.java
@@ -0,0 +1,14 @@
+package weeks.week_04;
+
+public class ContainsPractice {
+
+	public static void main(String[] args) {
+		String temp = "Inar Academy";
+		String temp2 = "Inar";
+		System.out.println("temp is " + temp);
+		System.out.println("temp2 is " + temp2);
+		System.out.println("Is temp contains temp2?");
+		System.out.println(temp.contains(temp2));
+	}
+
+}
