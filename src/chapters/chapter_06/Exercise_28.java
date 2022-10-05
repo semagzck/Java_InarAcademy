747665cc1779b6c9594a8a704ab7dc26a976e81b
Semanur <semagzckk@gmail.com>
2022-10-06 00:02:49 +0300

    Mersenne prime

---
+++ src/chapters/chapter_06/Exercise_28.java
@@ -0,0 +1,14 @@
+package chapter_06;
+
+public class Exercise_28 {
+	   public static boolean isMersenne(int num) {
+	        if (isPrime( (int) Math.pow(2, num) - 1)) return true;
+	        else return false;
+	    
+}
+
+	private static boolean isPrime(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
