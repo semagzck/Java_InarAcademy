fa724412dd50d3945a0686c4e2b05b76237d5801
Semanur <semagzckk@gmail.com>
2022-10-07 23:30:20 +0300

    Print calendar

---
+++ src/chapters/chapter_06/Exercise_34.java
@@ -0,0 +1,22 @@
+package chapter_06;
+
+public class Exercise_34 {
+	 public static void main(String[] args) {
+
+	System.out.print("Enter full year: ");
+  int year = in.nextInt();
+ 
+  System.out.print("Enter month as a number between 1 and 12: ");
+  int month = in.nextInt();
+  
+ for(int i = 1; i <= 12; i++) {
+	   printMonth(2015, i);
+  }
+	 }
+
+	private static void printMonth(int i, int i2) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
