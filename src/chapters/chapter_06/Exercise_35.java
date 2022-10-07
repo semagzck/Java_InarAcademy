96006d496677364a411ac350b1baa5b63cdb6245
Semanur <semagzckk@gmail.com>
2022-10-07 22:52:19 +0300

    Geometry:area of a pentagon

---
+++ src/chapters/chapter_06/Exercise_35.java
@@ -0,0 +1,26 @@
+package chapter_06;
+
+import java.util.Scanner;
+
+public class Exercise_35 {
+	public static void main(String[] args) {
+		  Scanner input= new Scanner(System.in);
+		  System.out.print("Enter the side:");
+		   
+		  double s = input.nextDouble();
+		   
+		  System.out.println("The area of the pentagon is " + area(s));
+		 
+		 }
+		  
+		  
+		public static double area(double side) {
+		   
+		  return 5*Math.pow(side, 2)/(4*Math.tan(Math.PI/5));
+		   
+		 }
+		 
+		
+}
+
+
