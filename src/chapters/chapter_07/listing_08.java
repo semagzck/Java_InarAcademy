16275c4d548ebae88ab2f147aecde92a7951e630
Semanur <semagzckk@gmail.com>
2022-10-09 22:26:04 +0300

    SelectionSort

---
+++ src/chapters/chapter_07/listing_08.java
@@ -0,0 +1,25 @@
+package chapter_07;
+
+public class listing_08 {
+	public class SelectionSort {
+		 public static void selectionSort(double[] list) {
+		 for (int i = 0; i < list.length - 1; i++) {
+		  double currentMin = list[i];
+		  int currentMinIndex = i;
+		
+		  for (int j = i + 1; j < list.length; j++) {
+		if (currentMin > list[j]) {
+		 currentMin = list[j];
+		 currentMinIndex = j;
+		 }
+		 }
+		
+		
+		 if (currentMinIndex != i) {
+		 list[currentMinIndex] = list[i];
+		 list[i] = currentMin;
+		 }
+		 }
+		 }
+		 }
+}
