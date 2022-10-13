623d82991678176edecd6d85ec49c7e12369c171
Semanur <semagzckk@gmail.com>
2022-10-13 18:30:05 +0300

    Game: locker puzzle

--- src/chapters/chapter_07/Exercise_23.java
+++ src/chapters/chapter_07/Exercise_23.java
@@ -2,22 +2,37 @@ package chapter_07;
 
 public class Exercise_23 {
 	 public static void main(String[] args) {
-	        boolean[] isLocked = new boolean[100];
+	        // false  door  closed
+	        boolean[] doors = new boolean[100];
 
-	        for (int i = 0; i < isLocked.length; i++) {
-	            for (int j = i; j < isLocked.length; j++) {
-	                if (isLocked[i]) {
-	                    isLocked[i] = false;
+	        int numberOfStudents = 100;
+
+	        updateDoors(numberOfStudents, doors);
+
+	        displayOpenDoor(doors);
+
+	    }
+
+	    public static void updateDoors(int numberOfStudents, boolean[] doors) {
+// diğer yolu dene??
+	        for (int student = 1; student <= numberOfStudents; student++) {
+	            for (int doorNumber = student - 1; doorNumber < doors.length; doorNumber += student) {
+
+	                if (doors[doorNumber]) {
+	                    doors[doorNumber] = false;
+	                } else {
+	                    doors[doorNumber] = true;
+
 	                }
-	                else isLocked[i] = true;
-	            }
-	        }
-	        for (int i = 0; i < isLocked.length; i++) {
-	            if (isLocked[i]) {
-	                System.out.print(i + " ");
 	            }
 	        }
 	    }
-	
-}
 
+	    public static void displayOpenDoor(boolean[] doors) {
+	        for (int i = 0; i < doors.length; i++) {
+	            if (doors[i]) {
+	                System.out.print((i + 1) + " ");
+	            }
+	        }
+	    }
+	}
