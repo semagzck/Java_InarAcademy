1cfb65d38f759ea08a69c3388f249b400df34572
Semanur <semagzckk@gmail.com>
2022-10-15 22:20:21 +0300

    added week10

---
+++ src/weeks/week_10/Student.java
@@ -0,0 +1,14 @@
+package weeks.week_10;
+
+public class Student {
+    String name;
+    String lastName;
+    int age;
+    String email;
+
+    public void changeEmail(String newEmail){
+        this.email = newEmail;
+    }
+
+
+}
