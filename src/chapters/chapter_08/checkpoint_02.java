a93c02f90d4f7f5be25d852a77a5f4257997961d
Semanur Gözüacik <semagzckk@gmail.com>
2022-10-19 00:28:36 +0300

    checkpoint

---
+++ src/chapters/chapter_08/checkpoint_02.java
@@ -0,0 +1,7 @@
+//Which of the following statements are valid?
+int[][] r = new int[2]; 	
+int[] x = new int[];
+int[][] y = new int[3][];
+int[][] z = {{1, 2}};
+int[][] m = {{1, 2}, {2, 3}};
+int[][] n = {{1, 2}, {2, 3}, };
