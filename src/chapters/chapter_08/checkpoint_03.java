3d7c4c4864e3ac1d8b4aeb5dabb3ad7c387c6f95
Semanur Gözüacik <semagzckk@gmail.com>
2022-10-19 00:29:33 +0300

    checkpoint

---
+++ src/chapters/chapter_08/checkpoint_03.java
@@ -0,0 +1,7 @@
+//Show the output of the following code:
+int[][] array = {{1, 2}, {3, 4}, {5, 6}};
+for (int i = array.length - 1; i >= 0; i--) {
+  for (int j = array[i].length - 1; j >= 0; j--)
+    System.out.print(array[i][j] + " ");
+  System.out.println();
+}
