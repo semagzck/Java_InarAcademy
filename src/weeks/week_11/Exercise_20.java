a50ca1f1fa78615a01e9aaaa2c6ff8719d7cac22
Semanur Gözüacik <semagzckk@gmail.com>
2022-10-23 23:25:25 +0300

    8.20

---
+++ src/weeks/week_11/Exercise_20.java
@@ -0,0 +1,44 @@
+package chapters.chapter_08;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Exercise_20 {
+    /**
+     * @Author Inar Academy
+     *
+     * * 1 - Anlat
+     * * 2 - Ornekle
+     * * 3 - Algoritmani sozel
+     * * 4 - Test case olustur
+     * * 5 - Kod yaz
+     * * 6 - test et
+     * <p>
+     * ANLAT
+     * 1 - 6 satir 7 sutunluk board
+     * 2 -  4 disks
+     * 2.1 - Same row
+     * 2.2 - Same col
+     * 2.3 - Diagonal right
+     * 2.4 - Diagonal left
+     * 3 - Oyuncular sirayla atiyor
+     * <p>
+     * ORNEKLE
+     * Kitap uzerinden konustuk
+     * <p>
+     * ALGORITMA
+     * 0 - Ask the user for input
+     * 1 - Validation check
+     * - user entry should be within the limits (col greater/lower)
+     * - user shouldn't enter a full column
+     * 2 - Process User's entry
+     * 3 - Display board
+     * 4 - is game over ?
+     * <p>
+     * TEST CASE
+     * <p>
+     * 1 - 00000000000
+     * 2 - Wining
+     * 3 - Play
+     */
+
