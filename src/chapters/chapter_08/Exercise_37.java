e1a4d6b19d310621f3719e7d2588c8c1174be6e1
Semanur Gözüacik <semagzckk@gmail.com>
2022-10-29 14:10:26 +0300

    Guess the capitals

---
+++ src/chapters/chapter_08/Exercise_37.java
@@ -0,0 +1,73 @@
+package chapters.chapter_08;
+import java.util.Scanner;
+public class Exercise_37 {
+
+        public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int correctCount = 0;
+          for (int i = 0; i < stateCapitals.length; i++) {
+            System.out.print("What is the capital of " + stateCapitals[i][0]+"? ");
+            String s = input.nextLine();
+            if (s.equalsIgnoreCase(stateCapitals[i][1])) {
+                System.out.println("Your answer is correct!");
+                correctCount++;
+            } else {
+                System.out.println("WRONG - The correct answer is " + stateCapitals[i][1]);
+            }
+        }
+        System.out.println("The correct count is " + correctCount);
+    }
+
+    private static String[][] stateCapitals = {
+            {"Alabama", "Montgomery"},
+            {"Alaska", "Juneau"},
+            {"Arizona", "Phoenix"},
+            {"Arkansas", "Little Rock"},
+            {"California", "Sacramento"},
+            {"Colorado", "Denver"},
+            {"Connecticut", "Hartford"},
+            {"Delaware", "Dover"},
+            {"Florida", "Tallahassee"},
+            {"Georgia", "Atlanta"},
+            {"Hawaii", "Honolulu"},
+            {"Idaho", "Boise"},
+            {"Illinois", "Springfield"},
+            {"Maryland", "Annapolis"},
+            {"Minnesota", "Saint Paul"},
+            {"Iowa", "Des Moines"},
+            {"Maine", "Augusta"},
+            {"Kentucky", "Frankfort"},
+            {"Indiana", "Indianapolis"},
+            {"Kansas", "Topeka"},
+            {"Louisiana", "Baton Rouge"},
+            {"Oregon", "Salem"},
+            {"Oklahoma", "Oklahoma City"},
+            {"Ohio", "Columbus"},
+            {"North Dakota", "Bismark"},
+            {"New York", "Albany"},
+            {"New Mexico", "Santa Fe"},
+            {"New Jersey", "Trenton"},
+            {"New Hampshire", "Concord"},
+            {"Nevada", "Carson City"},
+            {"Nebraska", "Lincoln"},
+            {"Montana", "Helena"},
+            {"North Carolina", "Raleigh"},
+            {"Missouri", "Jefferson City"},
+            {"Mississippi", "Jackson"},
+            {"Massachusetts", "Boston"},
+            {"Michigan", "Lansing"},
+            {"Pennsylvania", "Harrisburg"},
+            {"Rhode Island", "Providence"},
+            {"South Carolina", "Columbia"},
+            {"South Dakota", "Pierre"},
+            {"Tennessee", "Nashville"},
+            {"Texas", "Austin"},
+            {"Utah", "Salt Lake City"},
+            {"Vermont", "Montpelier"},
+            {"Virginia", "Richmond"},
+            {"Washington", "Olympia"},
+            {"West Virginia", "Charleston"},
+            {"Wisconsin", "Madison"},
+            {"Wyoming", "Cheyenne"}};
+    }
+
