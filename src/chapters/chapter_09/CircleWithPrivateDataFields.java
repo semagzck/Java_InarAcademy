20f77c2654f9156b78701d1e1a365dd618ba7057
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-01 23:47:18 +0300

    listing

---
+++ src/chapters/chapter_09/CircleWithPrivateDataFields.java
@@ -0,0 +1,34 @@
+package chapters.chapter_09;
+
+public class CircleWithPrivateDataFields {
+        private double radius = 1;
+
+        private static int numberOfObject = 0;
+
+        public CircleWithPrivateDataFields() {
+            numberOfObject++;
+        }
+
+        public CircleWithPrivateDataFields(double newRadius) {
+            radius = newRadius;
+            numberOfObject++;
+        }
+
+        public double getRadius() {
+            return radius;
+        }
+
+        public void setRadius(double newRadius) {
+            radius = (newRadius >= 0) ? newRadius : 0;
+        }
+
+        public static int getNumberOfObject() {
+            return numberOfObject;
+        }
+
+        public double getArea() {
+            return radius * radius * Math.PI;
+        }
+    }
+
+
