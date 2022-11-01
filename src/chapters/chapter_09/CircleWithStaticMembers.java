d2612196f37c80849ac5dc828502c3fd2423ab29
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-01 23:50:29 +0300

    listing

---
+++ src/chapters/chapter_09/CircleWithStaticMembers.java
@@ -0,0 +1,32 @@
+package chapters.chapter_09;
+
+public class CircleWithStaticMembers {
+    double radius;
+
+    static int numberOfObjects = 0;
+
+    
+    CircleWithStaticMembers() {
+        radius = 1;
+        numberOfObjects++;
+    }
+
+    CircleWithStaticMembers(double newRadius) {
+        radius = newRadius;
+        numberOfObjects++;
+    }
+
+
+    static int getNumberOfObjects() {
+        return numberOfObjects;
+    }
+
+ 
+    double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    double getRadius(){
+        return radius;
+    }
+}
