7177a8a65b7dff0b2aaf040db45f3b561662521f
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-05 12:47:45 +0300

    Stopwatch

---
+++ src/chapters/chapter_09/Exercise_06/StopWatch.java
@@ -0,0 +1,38 @@
+package chapters.chapter_09;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
+}
