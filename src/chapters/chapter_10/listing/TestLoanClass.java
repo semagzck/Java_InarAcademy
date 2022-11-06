af39732bd2f2fb29613a9b7db3cf7dd84b644477
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-06 22:22:24 +0300

    listing01

--- src/chapters/chapter_10/listing/TestLoanClass.java
+++ src/chapters/chapter_10/listing/TestLoanClass.java
@@ -1,71 +1,28 @@
 package chapters.chapter_10.listing;
+import java.util.Scanner;
 
-public class Loan {
+public class TestLoanClass {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+         System.out.print(
+                 "Enter annual interest rate, for example, 8.25: ");
+         double annualInterestRate = input.nextDouble();
 
-    private double annualInterestRate;
-  private int numberOfYears;
-  private double loanAmount;
-  private java.util.Date loanDate;
+         System.out.print("Enter number of years as an integer: ");
+         int numberOfYears = input.nextInt();
 
-          /** Default constructor tanımla*/
-          public Loan() {
-         this(2.5, 1, 1000);
-         }
+         // Enter loan amount
+         System.out.print("Enter loan amount, for example, 120000.95: ");
+         double loanAmount = input.nextDouble();
 
-        
-         public Loan(double annualInterestRate, int numberOfYears,
- double loanAmount) {
-         this.annualInterestRate = annualInterestRate;
-         this.numberOfYears = numberOfYears;
-         this.loanAmount = loanAmount;
-         loanDate = new java.util.Date();
-         }
+         // Create a Loan object
+         Loan loan =
+                 new Loan(annualInterestRate, numberOfYears, loanAmount);
 
-         /** Return annualInterestRate */
-         public double getAnnualInterestRate() {
-         return annualInterestRate;
-         }
-
-         public void setAnnualInterestRate(double annualInterestRate) {
-         this.annualInterestRate = annualInterestRate;
-         }
-
-         /** Return numberOfYears */
-         public int getNumberOfYears() {
-         return numberOfYears;
-         }
-
-         /** Set a new numberOfYears */
-         public void setNumberOfYears(int numberOfYears) {
-         this.numberOfYears = numberOfYears;
-         }
-
-         /** Return loanAmount */
-         public double getLoanAmount() {
-         return loanAmount;
-         }
-
-         /** Set a new loanAmount */
-         public void setLoanAmount(double loanAmount) {
-         this.loanAmount = loanAmount;
-         }
-
-         /** Find monthly payment */
-         public double getMonthlyPayment() {
-         double monthlyInterestRate = annualInterestRate / 1200;
-         double monthlyPayment = loanAmount * monthlyInterestRate / (1 -
-                 (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
-         return monthlyPayment;
-         }
-
-         /** Find total payment */
-         public double getTotalPayment() {
-         double totalPayment = getMonthlyPayment() * numberOfYears * 12;
-         return totalPayment;
-         }
-
-         /** Return loan date */
-         public java.util.Date getLoanDate() {
-         return loanDate;
+         // Display loan date, monthly payment, and total payment
+         System.out.printf("The loan was created on %s\n" +
+                         "The monthly payment is %.2f\nThe total payment is %.2f\n",
+                 loan.getLoanDate().toString(), loan.getMonthlyPayment(),
+                 loan.getTotalPayment());
          }
  }
