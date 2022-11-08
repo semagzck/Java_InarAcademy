8c42bd170fec661d62f9ec4ab94bdc4b0af3c22c
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-08 23:21:29 +0300

    added checkpoint

---
+++ src/chapters/chapter_10/checkpoints/checkpoint_01.java
@@ -0,0 +1,10 @@
+a.	Integer i = new Integer("23");
+b.	Integer i = new Integer(23);
+c.	Integer i = Integer.valueOf("23");
+d.	Integer i = Integer.parseInt("23", 8);
+e.	Double d = new Double();
+f.	Double d = Double.valueOf("23.45");
+g.	int i = (Integer.valueOf("23")).intValue();
+h.	double d = (Double.valueOf("23.4")).doubleValue();
+i.	int i = (Double.valueOf("23.4")).intValue();
+j.	String s = (Double.valueOf("23.4")).toString();
