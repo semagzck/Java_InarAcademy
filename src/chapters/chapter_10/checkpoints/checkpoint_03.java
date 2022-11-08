fcc41c37f8cfc24f941287a93b9b5effb0ed3ea3
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-08 23:24:43 +0300

    added checkpoint

---
+++ src/chapters/chapter_10/checkpoints/checkpoint_03.java
@@ -0,0 +1,13 @@
+package chapters.chapter_10.checkpoints;
+
+public class checkpoint_03 {
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt("10"));
+        System.out.println(Integer.parseInt("10", 10));
+        System.out.println(Integer.parseInt("10", 16));
+        System.out.println(Integer.parseInt("11"));
+        System.out.println(Integer.parseInt("11", 10));
+        System.out.println(Integer.parseInt("11", 16));
+    }
+}
+
