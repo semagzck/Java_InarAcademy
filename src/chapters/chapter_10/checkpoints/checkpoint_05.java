479b398bae3d127bdd4f19ecca9b5447969934d0
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-08 23:27:49 +0300

    added checkpoint

---
+++ src/chapters/chapter_10/checkpoints/checkpoint_05.java
@@ -0,0 +1,9 @@
+package chapters.chapter_10.checkpoints;
+
+public class checkpoint_05 {
+    public static void main(String[] args) {
+        Double x = 3.5;
+        System.out.println(x.intValue());
+        System.out.println(x.compareTo(4.5));
+    }
+}
