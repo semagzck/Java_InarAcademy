c09226e0ad3311df450f9bc70eda8c4a9f1cc52e
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-12 12:26:27 +0300

    TimeClass

---
+++ src/chapters/chapter_10/exercise/Exercise_01/Time.java
@@ -0,0 +1,44 @@
+package chapters.chapter_10.Exercise;
+
+public class Time {
+    private long hour;
+    private long minute;
+    private long second;
+
+    public Time(){
+        long totalSeconds = System.currentTimeMillis() / 1000;
+        long totalMinutes = totalSeconds / 60;
+        long totalHours = totalMinutes / 60;
+        this.hour = totalHours % 24;
+        this.minute = totalMinutes % 60;
+        this.second = totalSeconds % 60;
+    }
+    public Time(long elapsedTime) {
+        long totalSeconds = elapsedTime / 1000;
+        long totalMinutes = totalSeconds / 60;
+        long totalHours = totalMinutes / 60;
+        this.hour = totalHours % 24;
+        this.minute = totalMinutes % 60;
+        this.second = totalSeconds % 60;
+
+    }
+    public Time(long hour, long minute, long second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public long getHour() {
+        return hour;
+    }
+
+    public long getMinute() {
+        return minute;
+    }
+
+    public long getSecond() {
+        return second;
+    }
+
+
+}
