5d7f7d12aca6068f65a08a221e6ff1c84e4ff2e9
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-12 13:58:25 +0300

    Triangle2D

---
+++ src/chapters/chapter_10/exercise/Exercise_12/Triangle2D.java
@@ -0,0 +1,60 @@
+package chapters.chapter_10.Exercise;
+
+public class MyRectangle2D {
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+
+    public MyRectangle2D() {
+        this(0, 0, 1, 1);
+    }
+
+    public MyRectangle2D(double x, double y, double width, double height) {
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (height + width);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
