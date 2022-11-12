88336a21092e8d539be872482207d1cbde4be96f
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-12 12:27:17 +0300

    TestTimeClass

---
+++ src/chapters/chapter_10/exercise/Exercise_01/Exercise1TestTime.java
@@ -0,0 +1,11 @@
+package chapters.chapter_10.Exercise;
+
+public class Exercise1TestTime {
+
+    public  static void main(String[] args) {
+        Time time = new Time(555550000);
+        System.out.println(time.getHour() + ":" + time.getMinute() + ":" + time.getSecond());
+
+    }
+
+}
