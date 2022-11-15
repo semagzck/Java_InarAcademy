7962e9864a92f5805aa4a6084962433a7689d069
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-15 23:47:08 +0300

    listing

--- src/chapters/chapter_11/listing/SimpleGeometricObject.java
+++ src/chapters/chapter_11/listing/SimpleGeometricObject.java
@@ -1,46 +1,45 @@
 package chapters.chapter_11.listing;
 
+import java.util.Date;
+
 public class SimpleGeometricObject {
-    private String color = "white";
+    private String color;
     private boolean filled;
     private java.util.Date dateCreated;
 
-          public SimpleGeometricObject() {
-         dateCreated = new java.util.Date();
-         }
+    public SimpleGeometricObject(){
+        color="white";
+        filled=false;
+        dateCreated=new Date();
+    }
+    public SimpleGeometricObject(String color, boolean filled){
+        this.color=color;
+        this.filled=filled;
+        dateCreated=new Date();
 
-         public SimpleGeometricObject(String color, boolean filled) {
-             dateCreated = new java.util.Date();
-             this.color = color;
-             this.filled = filled;
-             }
+    }
 
-         public String getColor() {
-             return color;
-             }
+    public String getColor() {
+        return color;
+    }
 
-         public void setColor(String color) {
-             this.color = color;
-             }
+    public void setColor(String color) {
+        this.color = color;
+    }
 
+    public boolean isFilled() {
+        return filled;
+    }
 
-         public boolean isFilled() {
-             return filled;
-             }
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
 
-         public void setFilled(boolean filled) {
-             this.filled = filled;
-             }
-
-         /** Get dateCreated */
-         public java.util.Date getDateCreated() {
-             return dateCreated;
-             }
-
-         /** Return a string representation of this object */
-         public String toString() {
-             return "created on " + dateCreated + "\ncolor: " + color +
-                     " and filled: " + filled;
-             }
- }
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+    public String toString(){
+        return "created on " + dateCreated + "\ncolor: " + color + " and filled: " + filled;
+    }
+}
 
