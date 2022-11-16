95473d0fd68cb14075bae77a712f92df74f8ef47
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-16 11:49:35 +0300

    listing

---
+++ src/chapters/chapter_11/listing/CastingDemo.java
@@ -0,0 +1,28 @@
+package chapters.chapter_11.listing;
+
+public class CastingDemo {
+
+    public static void main(String[] args) {
+        Object object1 = new CircleFromSimpleGeometricObject(1);
+        Object object2 = new RectangleFromSimpleGeometricObject(1, 1);
+        display(object1);
+        display(object2);
+
+
+
+    }
+
+    private static void display(Object object) {
+        if(object instanceof CircleFromSimpleGeometricObject){
+            System.out.println("The circle area is " + ((CircleFromSimpleGeometricObject)object).getArea());
+            System.out.println("The circle diameter is " + ((CircleFromSimpleGeometricObject)object).getDiameter());
+
+        }
+        else if(object instanceof RectangleFromSimpleGeometricObject){
+            System.out.println("The rectangle area is " + ((RectangleFromSimpleGeometricObject)object).getArea());
+
+        }
+    }
+}
+
+
