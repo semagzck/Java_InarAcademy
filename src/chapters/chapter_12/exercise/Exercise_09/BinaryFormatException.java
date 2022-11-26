0caacc97e189643e2e34232885eaffabe2cf3a9b
Semanur Gözüacik <semagzckk@gmail.com>
2022-11-26 12:48:25 +0300

    Binary Format Exception

---
+++ src/chapters/chapter_12/exercise/Exercise_09/BinaryFormatException.java
@@ -0,0 +1,17 @@
+package chapters.chapter_12.exercise;
+
+public class BinaryFormatException extends Exception{
+    public BinaryFormatException(){
+        System.out.println("Wrong input!!!Entered number must be binary number.");
+    }
+
+}
+package chapters.chapter_12.exercise;
+
+public class BinaryFormatException extends Exception{
+	    public BinaryFormatException(){
+		            System.out.println("Wrong input!!!Entered number must be binary number.");
+			        }
+
+}
+
