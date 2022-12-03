7e21e2ea863f86d0123474fe4ab4968c78e369d6
Semanur Gözüacik <semagzckk@gmail.com>
2022-12-03 11:13:18 +0300

    Enable GeometricObject comparable

---
+++ src/chapters/chapter_13/exercise/Exercise_05/Circle.java
@@ -0,0 +1,22 @@
+package chapters.chapter_13.Exercise.Exercise_05;
+
+public class Circle extends ModifyGeometricObject {
+    double radius;
+    Circle(){
+        radius=1;
+    }
+    Circle(double newRadius){
+        radius=newRadius;
+    }
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+    public double getPerimeter(){
+        return 2*radius*Math.PI;
+    }
+    void setRadius(double newRadius){
+        radius=newRadius;
+    }
+
+}
+
