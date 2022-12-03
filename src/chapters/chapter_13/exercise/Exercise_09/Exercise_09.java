ae9970a7670d222a20f61761dea20418c4352ea7
Semanur Gözüacik <semagzckk@gmail.com>
2022-12-03 12:04:15 +0300

    Enable Circle comparable

---
+++ src/chapters/chapter_13/exercise/Exercise_09/Exercise_09.java
@@ -0,0 +1,15 @@
+package chapters.chapter_13.Exercise.Exercise_09;
+
+public class Exercise_09 {
+    public static void main(String[] args) {
+
+        Circle c1=new Circle(6);
+        Circle c2=new Circle(6);
+        Circle c3=new Circle(8);
+        System.out.println("Is c1 equals c2 ? "+c1.equals(c2));
+        System.out.println("Is c1 equals c3 ? "+c1.equals(c3));
+
+
+    }
+}
+
