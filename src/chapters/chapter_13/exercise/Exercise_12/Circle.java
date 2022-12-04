e4a51a3a9a7b66b2225baeb6290614b496e39e73
Semanur Gözüacik <semagzckk@gmail.com>
2022-12-04 22:01:48 +0300

    Circle class

---
+++ src/chapters/chapter_13/exercise/Exercise_12/Circle.java
@@ -0,0 +1,35 @@
+package chapters.chapter_13.Exercise.Exercise_12;
+
+public class Circle extends GeometricObject {
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+}
